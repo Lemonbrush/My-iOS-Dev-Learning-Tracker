caff461ae65e680c405f8a91ee72686490a3145c
Lemonbrush <aa_rubtsov@mail.ru>
2021-02-01 21:24:57 +0300

    Comments to Stack Maximum Elements added

--- Data_structures/Stack Maximum Element/Stack Maximum Element/main.swift
+++ Data_structures/Stack Maximum Element/Stack Maximum Element/main.swift
@@ -1,3 +1,16 @@
+/*
+ You have an empty sequence, and you will be given N queries. Each query is one of these three types:
+ 
+ 1 x  -Push the element x into the stack.
+ 2    -Delete the element present at the top of the stack.
+ 3    -Print the maximum element in the stack.
+ 
+ Input Format
+ The first line of input contains an integer, N. The next N lines each contain an above mentioned query. (It is guaranteed that each query is valid.)
+ 
+ Output Format
+ For each type 3 query, print the maximum element in the stack on a new line.
+ */
 
 import Foundation
 
